4fe1732545086a30449f67382ac862f9da37962f
iw545 <leo.ihalainen@windowslive.com>
2025-07-26 23:16:59 +0300

    viikko3 palautus, logiikan kehitystä

---
+++ dokumentaatio/viikkoraportit/viikko3.md
@@ -0,0 +1,42 @@
+# Viikkoraportti 2
+
+---
+
+Tällä viikolla käytin aikaa pelkästään asioiden hahmotukseen vielä paljon, vieläkään en
+täysin varma olenko ymmärtänyt kaikkea oikein. Lähdin toteuttamaan ohjelman logiikkaa
+siten miten itse tulkitsin ja ymmärsin suurimmaksi osaksi. Opin paremmin, tai ainakin
+luulisin niin, Markovin Ketjujen toiminnasta ja logiikasta, piti samaa tekstiä lukea
+monta kertaa ennenkuin paremmin aloin hahmottamaan millä tavalla itse koodia tulisi
+kirjoittaa millä yritän hakea samaa ideaa kuin aiheen dokumentissa niiden toiminnasta.
+
+Epäselväksi jäi yksikkötestaaminen, tarkemmin tuo miten saisin ne toimimaan ohjelmassani
+missä testeille pitäisi valmis input syöte syöttää, ettei testien aikana tarvisi niitä
+syöttää manuaalisesti. Jotain selvästi en osannut tässä hahmottaa oikein, yritin muutamaa
+eri tapaa saada ne toimimaan mutta en onnistunut ja niiden kohdalla päätin olla tällä
+viikolla käyttämättä enempää aikaa kun oli paljon epävarmuutta asiasta. Yksikkötestejä
+ei ole tällä hetkellä vieläkään toimivia, niitä pitää paljon ensi viikolla tehdä,
+kunhan toivon mukaan ymmärrän niiden toimintatavan. Myöskään siis testausdokumentin
+kirjoitusta en ole aloittanut tämän takia.
+
+Koska jätin logiikan kehittämistä paljon tälle viikkopalautuspäivän viimeiselle tunneille, en
+ehtinyt selvittämään miksi logiikka ei tällä hetkellä toimi niinkuin tarkoitus
+(aina pelaaja / tietokone voittaa joka kerta saman vaihtoehdon valitessa). Odotan
+ensin palautetta ohjaajalta onko ok suuntaan ohjelma menossa ennenkuin yritän korjata.
+
+---
+
+Kysymyksiä ohjaajalle:
+
+Miten yksikkötestien kohdalla tulisi edetä, vaikeata oli löytää kunnon dokumentaatiota miten tehdä
+nuo oikeaoppisesti, tulisiko käyttää mock moduulia unittest kirjastosta vai jotain muuta menetelmää käyttää?
+Yritin esim tästä sivusta apua katsoa, muttei onnistunut:
+https://stackoverflow.com/questions/21046717/how-can-i-mock-user-input-from-input-in-3-x-or-raw-input-in-2-x-for-a-uni
+
+Pääohjelman kannalta, onko logiikka menossa oikeaan suuntaan, pitäisikö jotain rakenteessa / logiikassa muuttaa?
+En itse osaa oikein hahmottaa onko tämä tällä hetkellä Markovin ketjujen logiikkaa normaalisti noudattavaa koodia.
+
+---
+
+Käytin aikaa tällä viikolla noin 13 tuntia tämän projektin parissa. Suurin osa tästä meni
+asioiden miettimiseen ja hahmottamiseen toteutuksen kannalta ja osa ajasta yksikkötestien 
+ongelmien korjaamiseen, mikä ei tuottanut tulosta kuitenkaan.
--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
-#alkuun väliaikainen yksinkertainen tapa tietokoneella valita oma
-#vaihtoehto sattumanvaraisesti (kivi / sakset / paperi)
 import random
 
 class RPS:
-    def __init__(self):
+    def __init__(self, markovs):
         self.options = ["1", "2", "3", "4"]
-        self.results = []
+        self.letters = ["", "R", "P", "S"]
         self.points = {"Pelaaja": 0, "Tietokone": 0}
+        self.history = ""
+        self.markovs = markovs
+        self.current_markov = random.choice(self.markovs)
+        self.best = sum(self.current_markov.results)
+        self.focus = 5
 
     def play(self):
 
@@ -20,20 +23,37 @@ class RPS:
             print("Valitse uudestaan (1/2/3/4)")
             continue
 
+        #tallennetaan pelaajan valinnat
+        self.history += self.letters[option]
+
         #vertaa pelaajan valintaa tietokoneen valintaan
         result = self.compare(option)
-        self.results.append(result)
 
-        if option == result:
+        if result == 0:
+            print("Tasapeli")
+            self.current_markov.results.append(0)
+        elif option == result:
             print("Kierroksen voitti Pelaaja")
             self.points["Pelaaja"] += 1
+            self.current_markov.results.append(-1)
         else:
             print("Kierroksen voitti Tietokone")
             self.points["Tietokone"] += 1
+            self.current_markov.results.append(1)
 
         print("Pistetilanne:")
         for x, y in self.points.items():
             print(x, y)
+
+        #päivitä tieto mitä vaihtoehtoa pelaaja käytti
+        if len(self.history) > 1:
+            self.current_markov.matrix[self.history[-2:]][self.letters[option]] += 1
+
+        #vaihda markov mallia 5 edellisen kierroksen pisteiden perusteella jos tarve
+        for i in self.markovs:
+            if sum(i.results) > self.best:
+                self.best = sum(i.results)
+                self.current_markov = i
 
         self.play()
 
@@ -64,16 +84,59 @@ class RPS:
             case (2,3):
                 result = 2
 
-        #päivitä siirtymähistoria (esim kiven jälkeen paperi)
-
         return result
 
     def computer_choose(self):
-        #väliaikainen ratkaisu
-        choice = random.choice([1,2,3])
+        if len(self.history) < 2:
+            choice = random.choice([1,2,3])
+            return choice
+
+        choice = 1
+        frequencies = self.current_markov.matrix.get(self.history[-2:])
+        predict_move = max(frequencies.values())
+
+        #muuta vaihtoehto sen mukaan minkä pelaaja todennäköisimmin valitsee
+        if predict_move == "R":
+            choice = 3
+        elif predict_move == "P":
+            choice = 2
+        else:
+            choice = 3
 
         return choice
 
+class MarkovChain:
+    def __init__(self, results, matrix):
+        self.results = results
+        self.matrix = matrix
+
 #testaus
-game = RPS()
+#results 1 = voitto, 0 = tasapeli, -1 = häviö
+'''dictionary rakenteessa ai vertaa minkä 2 kierroksen yhdistelmän jälkeen
+pelaaja on pelannut seuraavaksi (esim kivi, kivi jälkeen joko kivi / paperi / sakset, jokaisella
+oma arvo merkkaa monta kertaa pelaaja on valinnut sen vaihtoehdon)
+'''
+markov1 = MarkovChain([1,0,1,1,-1], {"RR": {"R": 2, "P": 1, "S": 0},
+                                     "RS": {"R": 0, "P": 0, "S": 1},
+                                     "RP": {"R": 0, "P": 1, "S": 0},
+                                     "PR": {"R": 1, "P": 3, "S": 0},
+                                     "PP": {"R": 2, "P": 2, "S": 0},
+                                     "PS": {"R": 1, "P": 0, "S": 0},
+                                     "SR": {"R": 0, "P": 0, "S": 0},
+                                     "SP": {"R": 1, "P": 0, "S": 0},
+                                     "SS": {"R": 0, "P": 2, "S": 3}})
+
+markov2 = MarkovChain([0,-1,1,0,0], {"RR": {"R": 0, "P": 1, "S": 1},
+                                     "RS": {"R": 1, "P": 0, "S": 1},
+                                     "RP": {"R": 1, "P": 1, "S": 0},
+                                     "PR": {"R": 1, "P": 0, "S": 0},
+                                     "PP": {"R": 2, "P": 0, "S": 1},
+                                     "PS": {"R": 0, "P": 0, "S": 3},
+                                     "SR": {"R": 0, "P": 1, "S": 0},
+                                     "SP": {"R": 0, "P": 0, "S": 0},
+                                     "SS": {"R": 1, "P": 2, "S": 0}})
+
+markov_models = [markov1, markov2]
+
+game = RPS(markov_models)
 game.play()
